092b8606fdd1194271945343f544a35709ad25c4
Kim Pepper <kim@pepper.id.au>
2011-01-07 07:25:16 +1100

    Added support for Drupal 7 private directory

--- lib/capistrano-drupal.rb
+++ lib/capistrano-drupal.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :runner_group, "www-data"
   
   set(:deploy_to) { "/var/www/#{application}" }
-  set :shared_children, ['files']
+  set :shared_children, ['files', 'private']
   
   set(:db_root_password) {
     Capistrano::CLI.ui.ask("MySQL root password:")
@@ -63,7 +63,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Symlink settings and files to shared directory. This allows the settings.php and \
       and sites/default/files directory to be correctly linked to the shared directory on a new deployment."
     task :symlink_shared do
-      ["files", "settings.php"].each do |asset|
+      ["files", "private", "settings.php"].each do |asset|
         run "rm -rf #{app_path}/#{asset} && ln -nfs #{shared_path}/#{asset} #{app_path}/sites/default/#{asset}"
       end
     end
