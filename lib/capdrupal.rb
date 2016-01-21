c675196037ca54042ddd73b861f266dbe3b6d06b
Gilles Doge <gilles@antistatique.net>
2016-01-21 19:04:44 +0100

    use the HTTPS version for downloading drush + follow redirect

--- lib/capdrupal.rb
+++ lib/capdrupal.rb
@@ -183,7 +183,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :drush do
     desc "Gets drush and installs it"
     task :get, :roles => :app, :except => { :no_release => true } do
-      run "#{try_sudo} cd #{shared_path} && curl -O -s http://ftp.drupal.org/files/projects/drush-7.x-5.8.tar.gz && tar -xf drush-7.x-5.8.tar.gz && rm drush-7.x-5.8.tar.gz"
+      run "#{try_sudo} cd #{shared_path} && curl -LO -s https://ftp.drupal.org/files/projects/drush-7.x-5.8.tar.gz && tar -xf drush-7.x-5.8.tar.gz && rm drush-7.x-5.8.tar.gz"
       run "#{try_sudo} cd #{shared_path} && chmod u+x drush/drush"
     end
 
